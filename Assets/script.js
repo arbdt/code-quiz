50d07138d0e7b0ca740795bcf9fa17329326b5fd
arbdt <67365149+arbdt@users.noreply.github.com>
2020-09-05 18:54:36 +0930

    played around with styling and layout while contemplating how quiz program will work

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 // link with html elements
 var quizQuestion = document.getElementById("questionTitle");
-var quizOptions = document.getElementById("answerOptions");
+var quizOptions = document.getElementById("answerArea");
 var timerDisplay = document.getElementById("timerDisplay");
 var startButton = document.getElementById("startButton");
 
@@ -10,15 +10,18 @@ var quizQnArray = ["Q1: Text Here",
     "Q3",
     "Q4",
     "Q5",];
-var q1_answers = [];
+var q1_answers = ["answer 1", "answer 2", "answer 3", "answer 4"];
 var q2_answers = [];
 var q3_answers = [];
 var q4_answers = [];
 var q5_answers = [];
 
 // other variables
-var timeLeft = 120;
+var playerStorage = window.localStorage;
+var timeLeft = 75;
 var interval;
+var qnCorrect = false;
+var isPlaying = true;
 
 // reset to defaults
 function restartQuiz(){
@@ -29,10 +32,39 @@ function restartQuiz(){
 //restartQuiz();
 
 // start quiz on button
-startButton.addEventListener("click", playQuiz());
+startButton.onclick(playQuiz());
 
 function playQuiz(){
-    //startButton.hidden = true;
-    quizQuestion.textContent = quizQnArray[0];
     timerDisplay.textContent = timeLeft;
+    //startButton.hidden = true;
+    for (var i = 0; i < quizQnArray.length; i++){
+        poseQuestion(i);
+    }
+
+}
+
+function poseQuestion(qnNumber){
+    quizQuestion.textContent = quizQnArray[qnNumber];
+    var answerPool;
+    if (qnNumber == 0){
+        answerPool = q1_answers;
+    }
+    else if (qnNumber == 1){
+        answerPool = q2_answers;
+    }
+    else if (qnNumber == 2){
+        answerPool = q3_answers;
+    }
+    else if (qnNumber == 3){
+        answerPool = q4_answers;
+    }
+    else if (qnNumber == 4){
+        answerPool = q5_answers;
+    }
+    for (var i =0; i < answerPool.length; i++){
+        var answerOption = document.createElement("button");
+        answerOption.setAttribute("id", `option${i}`);
+        answerOption.textContent = answerPool[i];
+        quizOptions.appendChild(answerOption);
+    }
 }
