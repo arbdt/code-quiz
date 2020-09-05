66494461bea9faab4e8f16a2bfab3fe1630e29d0
arbdt <67365149+arbdt@users.noreply.github.com>
2020-09-05 21:00:16 +0930

    changed answer placeholder to <li> <a> style, added styles for same, added dummy answers and questions

--- Assets/script.js
+++ Assets/script.js
@@ -5,16 +5,16 @@ var timerDisplay = document.getElementById("timerDisplay");
 var startButton = document.getElementById("startButton");
 
 // quiz questions and answers
-var quizQnArray = ["Q1: Text Here",
-    "Q2",
-    "Q3",
-    "Q4",
-    "Q5",];
-var q1_answers = ["answer 1", "answer 2", "answer 3", "answer 4"];
-var q2_answers = [];
-var q3_answers = [];
-var q4_answers = [];
-var q5_answers = [];
+var quizQnArray = ["Q1: Lorem Ipsum?",
+    "Q2: Dolor Sit?",
+    "Q3: Amet Consectetur?",
+    "Q4: Adipisicing Elit?",
+    "Q5: Sed Do?",];
+var q1_answers = ["It", "Was", "The", "Worst"];
+var q2_answers = ["Of", "Times", "It", "Was"];
+var q3_answers = ["The", "Winter", "Of", "Our"];
+var q4_answers = ["Disco", "Tent", "C", "D"];
+var q5_answers = ["E", "F", "G", "H"];
 
 // other variables
 var playerStorage = window.localStorage;
@@ -32,9 +32,10 @@ function restartQuiz(){
 //restartQuiz();
 
 // start quiz on button
-startButton.onclick(playQuiz());
+startButton.addEventListener("click", playQuiz());
 
 function playQuiz(){
+    isPlaying = true; // set playing status true
     timerDisplay.textContent = timeLeft;
     //startButton.hidden = true;
     for (var i = 0; i < quizQnArray.length; i++){
@@ -62,9 +63,19 @@ function poseQuestion(qnNumber){
         answerPool = q5_answers;
     }
     for (var i =0; i < answerPool.length; i++){
-        var answerOption = document.createElement("button");
+        var answerOption = document.createElement("a");
         answerOption.setAttribute("id", `option${i}`);
+        answerOption.setAttribute("href", "#");
         answerOption.textContent = answerPool[i];
-        quizOptions.appendChild(answerOption);
+        quizOptions.children[i].appendChild(answerOption);
     }
 }
+
+// LOGICAL FLOW: 
+// Start Quiz -> start timer (every 1000ms, timer -= 1 until 0 OR !playing)
+// SET Q1 Qn and Q1 answers to display on HTML 
+// GET answer clicked -> decide if correct OR !correct
+// IF !correct -> timer penalty
+// SET Q2 Qn and Q2 answers to display
+// repeat until time = 0 OR !playing (finished)
+// Save Player {Name: [entry], Score: [time-remaining] to LocalState}
