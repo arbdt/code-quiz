9d8e415d486e9830dbf17be63bbb356dac40c9bf
arbdt <67365149+arbdt@users.noreply.github.com>
2020-09-06 12:24:26 +0930

    worked on answer choice logic

--- Assets/script.js
+++ Assets/script.js
@@ -5,16 +5,16 @@ var timerDisplay = document.getElementById("timerDisplay");
 var startButton = document.getElementById("startButton");
 
 // quiz questions and answers
-var quizQnArray = ["Q1: Lorem Ipsum?",
-    "Q2: Dolor Sit?",
-    "Q3: Amet Consectetur?",
-    "Q4: Adipisicing Elit?",
-    "Q5: Sed Do?",];
-var q1_answers = ["It", "Was", "The", "Worst"];
-var q2_answers = ["Of", "Times", "It", "Was"];
-var q3_answers = ["The", "Winter", "Of", "Our"];
-var q4_answers = ["Disco", "Tent", "C", "D"];
-var q5_answers = ["E", "F", "G", "H"];
+var quizQnArray = ["Q1: Commonly used data types do not include ...",
+    "Q2: The condition of a loop is enclosed in ...",
+    "Q3: Arrays in JavaScript can be used to store ...",
+    "Q4: String values must be enclosed within...",
+    "Q5: To print to console in JavaScript, the syntax is...",];
+var q1_answers = ["Strings", "Booleans", "Alerts", "Numbers"];
+var q2_answers = ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"];
+var q3_answers = ["Numbers", "Strings", "Objects", "All of the above"];
+var q4_answers = ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"];
+var q5_answers = ["Print()", "Console.log()", "System.out.Println()", "cout <<"];
 
 // other variables
 var playerStorage = window.localStorage; // storage of scores
@@ -67,19 +67,91 @@ function poseQuestion(qnNumber){ //to show question and answers
         answerPool = q5_answers;
     }
     for (var i =0; i < answerPool.length; i++){ //for each answer in set
-        var answerOption = document.createElement("a"); // create an answer element
-        answerOption.setAttribute("id", `option${i}`);
-        answerOption.setAttribute("class", "answerLink");
-        answerOption.setAttribute("href", "#");
-        answerOption.textContent = answerPool[i];
-        answerOption.style = "display: block";
         quizOptions.children[i].style = "display: list-item"; // show answer list to user
-        quizOptions.children[i].appendChild(answerOption); // add answers to answer area list
-        console.log(answerOption);
+        quizOptions.children[i].firstChild.textContent = answerPool[i]; // add answers to answer area list
+        quizOptions.children[i].firstChild.addEventListener("click",choseAnswer);
+        console.log(answerPool[i]);
     }
 }
 
+// answer clicked
+function choseAnswer(){
+    var qnAnswered = false;
+    var answerChosen = event.currentTarget; //get object being listened to
+       while (!qnAnswered){
+            // correct / incorrect answer logic
+            if (numQuestion == 1 && answerChosen.innerText == "Alerts"){ // question 1
+                console.log(answerChosen.innerText);
+                document.getElementById("qnResult").textContent = "Correct!";
+                qnAnswered = true;
+            }
+            else if (numQuestion == 1 && answerChosen.innerText != "Alerts"){
+                console.log(answerChosen.innerText);
+                console.log(answerChosen);
+                document.getElementById("qnResult").textContent = "Incorrect!";
+                timeLeft -= 15;
+                qnAnswered = true;
+            }
+            if (numQuestion == 2 && answerChosen.textContent == "Parentheses"){ // question 2
+                console.log(answerChosen.textContent); 
+                document.getElementById("qnResult").textContent = "Correct!";
+                qnAnswered = true;
+            }
+            else if (numQuestion == 2 && answerChosen.textContent != "Parentheses"){
+                console.log("incorrect");
+                document.getElementById("qnResult").textContent = "Incorrect!";
+                timeLeft -= 15;
+                qnAnswered = true;
+            }
+            if (numQuestion == 3 && answerChosen.textContent == "All of the above"){ // question 3
+                console.log(answerChosen.textContent);
+                document.getElementById("qnResult").textContent = "Correct!";
+                qnAnswered = true;
+            }
+            else if (numQuestion == 3 && answerChosen.textContent != "All of the above"){
+                console.log("incorrect");
+                document.getElementById("qnResult").textContent = "Incorrect!";
+                timeLeft -= 15;
+                qnAnswered = true;
+            }
+            if (numQuestion == 4 && answerChosen.textContent == "Quotes"){ //question 4
+                console.log(answerChosen.textContent);
+                document.getElementById("qnResult").textContent = "Correct!";
+                qnAnswered = true;
+            }
+            else if (numQuestion == 4 && answerChosen.textContent != "Quotes"){
+                console.log("incorrect");
+                document.getElementById("qnResult").textContent = "Incorrect!";
+                timeLeft -= 15;
+                qnAnswered = true;
+            }
+            if (numQuestion == 5 && answerChosen.textContent == "Console.log()"){ //question 5
+                console.log(answerChosen.textContent);
+                document.getElementById("qnResult").textContent = "Correct!";
+                qnAnswered = true;
+            }
+            else if (numQuestion == 5 && answerChosen.textContent != "Console.log()"){
+                console.log("incorrect");
+                document.getElementById("qnResult").textContent = "Incorrect!";
+                timeLeft -= 15;
+                qnAnswered = true;
+            }
+        }
+        if (qnAnswered && numQuestion < 5){
+            numQuestion += 1; // move on to next question
+            poseQuestion(numQuestion);
+        }
+        else if (timeLeft <= 0 || numQuestion == 5){
+            isPlaying = false;
+            clearInterval();
+        }
+}
+
+
 // reset to defaults
+function restartQuiz(){
+
+}
 
 
 // high scores
